7d49df53827fd05bb43e8afa1abbc1a4ab210c3f
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2016-05-24 18:37:51 +0200

    add filtering in stations list view

--- modules/stations/client/directives/station-list-entity.client.directive.js
+++ modules/stations/client/directives/station-list-entity.client.directive.js
@@ -3,13 +3,13 @@
 
   angular
     .module('stations')
-    .directive('stationListEntity', stationListEntity);
+    .directive('station', station);
 
-  stationListEntity.$inject = [/*Example: '$state', '$window' */];
+  station.$inject = [/*Example: '$state', '$window' */];
 
-  function stationListEntity(/*Example: $state, $window */) {
+  function station(/*Example: $state, $window */) {
     return {
-      templateUrl: 'views/station-list-entity.client.html',
+      templateUrl: '/modules/stations/client/views/station-list-entity.client.html',
       restrict: 'E',
       link: function postLink(scope, element, attrs) {
       }

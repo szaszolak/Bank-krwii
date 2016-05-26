e67905911ab86bf541d46b90cfa06a585cd1e44a
Mikolaj Wawrzyniak <g.m.wawrzyniak@gmail.com>
2016-05-26 20:05:28 +0200

    fix not refreshin user variables

--- modules/stations/client/controllers/list-stations.client.controller.js
+++ modules/stations/client/controllers/list-stations.client.controller.js
@@ -5,13 +5,13 @@
     .module('stations')
     .controller('StationsListController', StationsListController);
 
-  StationsListController.$inject = ['StationsService'];
+  StationsListController.$inject = ['StationsService','Authentication'];
 
-  function StationsListController(StationsService) {
+  function StationsListController(StationsService,Authentication) {
     var vm = this;
     vm.blood_types = ["0+","0-","A+","A-","B+","B-","AB+","AB-"];
     vm.stations = StationsService.query();
-    vm.isManager = user.roles.includes("manager");
+    vm.isManager = Authentication.user.roles.includes("manager");
     vm.user_station = user.station;
   }
 })();
